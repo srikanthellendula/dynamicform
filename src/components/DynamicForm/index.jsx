185028180aabd9249939e5ee43f2e648eb394d35
Srikanth Ellendula <srikanthellendula48@gmail.com>
2024-04-25 16:11:47 +0530

    validations

--- src/components/DynamicForm/index.jsx
+++ src/components/DynamicForm/index.jsx
@@ -8,10 +8,11 @@ const DynamicForm = () =>{
     const [form, setForm] = useState([])
     const [optionVal, setOptionVal] = useState('')
     const [options, setOptions] = useState([])
-    const [isLabelEmpty , setLabelStatus] = useState(true) 
+    const [isLabelEmpty , setLabelStatus] = useState(true)
+    const [isOptionEmpty, setOptionEmptyStatus] = useState(true)
     const [isSubmitted, setSubmitStatus] = useState(false)
 
-    const [userText, setUserText] = useState('')
+     
 
      
 
@@ -35,27 +36,32 @@ const DynamicForm = () =>{
             setForm(prev=>([...prev, newData]))
             setLabelText('')   
             setOptions([])
+            setOptionVal('')
             setSubmitStatus(true)
-            setInputType('none')   
+            setLabelStatus(true)
         } 
               
     }
 
     const onFormSubmit = (event) =>{
-        event.preventDefault()
-        console.log(userText)
+        event.preventDefault()        
         console.log(form)
         setForm([])
         setSubmitStatus(false)
-        setInputType('none')      
+        setLabelStatus(true)   
 
     }
 
-    const AddingOPtions = () => {
-            if (optionVal !== ''){
+    const AddingOptions = () => {
+        if (options.length === 0){
+            setOptionEmptyStatus(false)
+             
+        }
+        if (optionVal !== ''){
                 setOptions([...options, optionVal]);
+                setOptionEmptyStatus(true)
                 setOptionVal('')
-            }    
+        }    
    }
  
     return(
@@ -77,19 +83,21 @@ const DynamicForm = () =>{
             <>
             <label htmlFor='labelText'> Enter Label </label> <br/>            
             <input id='labelText' value={labelText} type='text' onChange={(event)=>{setLabelText(event.target.value), setLabelStatus(true)}}  />  <br/> 
-            {/* {isLabelEmpty === false && <p className='err-msg'> * Label not to be empty </p>} */}
+            {isLabelEmpty === false && <p className='err-msg'> * Label required </p>}
             <button onClick={addToForm}> Add to Form </button>         
             </>            
             }
             {(inputType === 'radio' || inputType === 'select' || inputType === 'checkbox')&& 
             <>
             <label htmlFor='labelText'> Enter Label </label> <br/>
+            {isLabelEmpty === false && <p className='err-msg'> * required </p>}
             <input id='labelText' value={labelText} type='text' onChange={(event)=>{setLabelText(event.target.value), setLabelStatus(true)}}  /> <br/>
-            {/* {isLabelEmpty === false && <p className='err-msg'> * Label not to be empty </p>} */}
-            <label> Add Options  </label> <br/>
-            <input type='text' value={optionVal}  onChange={(e)=>setOptionVal(e.target.value)}/>
             
-            <button onClick={AddingOPtions} > Add Choices  </button> <br/>
+            <label> Add Options  </label> <br/>
+            <input type='text' value={optionVal}  onChange={(e)=>{setOptionVal(e.target.value), setOptionEmptyStatus(true)}}/>
+            
+            <button onClick={AddingOptions} > Add Choices  </button> <br/>
+            {isOptionEmpty === false && <p className='err-msg'> * at least one option required </p>}
             {options.map(eachItem=> <li key={v4()} > {eachItem} </li>)}
             <button onClick={addToForm}> Add to Form </button>         
             </>            
@@ -121,15 +129,15 @@ const DynamicForm = () =>{
                         (eachItem.type==='radio' || eachItem.type==='checkbox') &&
                         <>
                             <label> {eachItem.label}</label> <br/>
-                            {eachItem.options.map(eachValue=><> <input required onChange={(event)=>{setFinalValue(event.target.value)}} name={eachItem.label} id={eachValue} type={eachItem.type}/> <label htmlFor={eachValue}> {eachValue} </label>  </>)} <br/>
+                            {eachItem.options.map(eachValue=><> <input required name={eachItem.label} id={eachValue} type={eachItem.type}/> <label htmlFor={eachValue}> {eachValue} </label>  </>)} <br/>
                         </>
                     }
                     {
                         (eachItem.type==='select') &&
                         <>
                             <label> {eachItem.label}</label> <br/>
-                            <select>
-                            {eachItem.options.map(eachValue=> <option> {eachValue} </option>)} 
+                            <select onChange={(e)=> setUserText(e.target.value)}>
+                            {eachItem.options.map(eachValue=> <option value={eachValue}> {eachValue} </option>)} 
                             </select> <br/>
                             
                         </>
